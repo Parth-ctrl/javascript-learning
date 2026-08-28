02f4addecffa899d3b72e64ce77f699c3aca32d3
Parth Saini <sainiparth8@gmail.com>
2026-08-28 17:57:57 +0530

    Function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,13 +28,44 @@
 // console.log("Result:", result);
 
 
-function loginMessage(userName="Hitesh"){
-    if(!userName){
-        console.log("UserName Not Found")
-    }
-    else{
-    console.log(`${userName}, Is Logged In`)
-    }
+// function loginMessage(userName="Hitesh"){
+//     if(!userName){
+//         console.log("UserName Not Found")
+//     }
+//     else{
+//     console.log(`${userName}, Is Logged In`)
+//     }
+// }
+
+// loginMessage("Parth")
+
+
+// function calculatePrice(...num){
+//     return num;
+// }
+
+// console.log(calculatePrice(2, 4))
+
+// const user = {
+//     userName: "Parth",
+//     price : 12
+// }
+
+// function handleUser(anyObject){
+//     console.log(`UserName is ${anyObject.userName} and price is ${anyObject.price}`)
+// };
+
+// // handleUser(user);
+// handleUser({
+//     userName: "Parth",
+//     price : 12
+// });
+
+const Newuser = [200, 400, 600];
+
+function returnSecondValue(getArray){
+    return `Second Value: ${getArray[1]}`
 }
 
-loginMessage("Parth")
+// console.log(returnSecondValue(Newuser))
+console.log(returnSecondValue([200, 400, 600]))
