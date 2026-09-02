98d5df3f37f3620071d3ca2547b4ca47eecc417b
Parth Saini <sainiparth8@gmail.com>
2026-09-02 10:43:01 +0530

    Scopes

---
+++ 03_basics/02_socopes.js
@@ -0,0 +1,43 @@
+// let a = 200;
+
+// if (true) {
+//     console.log(a);
+// }
+
+// if(true){
+// let a = 10;
+// const b = 20;
+// // var c = 30;
+// console.log("Inner", a);
+// }
+
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+
+// function one(){
+//    const userName = "Parth";
+
+//    function two(){
+//     const website = "Youtube";
+//     console.log(userName);
+//    }
+
+// //    console.log(website);
+
+//    two()
+// }
+
+// one()
+
+if(true){
+   const userName = "Parth";
+   if(userName === "Parth"){
+       const website = "Youtube";
+       console.log(userName +" " +website);
+   }
+   // console.log(website);
+}
+
+// console.log(userName);
